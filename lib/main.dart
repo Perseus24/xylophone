8ce01eb11ac4021b42ba64c4f7c6a0c2dba19d65
Cy Jay Herrera <herreracyjay24@gmail.com>
2024-01-17 10:16:25 +0800

    feat(project): Added 7 buttons for the sound files

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,81 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: TextButton(
-            onPressed: (){
-              final load = AudioPlayer();
-              load.play(AssetSource('note1.wav'));
-            },
-            child: Text("Press me!"),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.red
+                ),
+                onPressed: (){
+                  final load = AudioPlayer();
+                  load.play(AssetSource('note1.wav'));
+                },
+                child: Text("Press me!"),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                    backgroundColor: Colors.orange
+                ),
+                onPressed: (){
+                  final load = AudioPlayer();
+                  load.play(AssetSource('note2.wav'));
+                },
+                child: Text("Press me!"),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                    backgroundColor: Colors.yellow
+                ),
+                onPressed: (){
+                  final load = AudioPlayer();
+                  load.play(AssetSource('note3.wav'));
+                },
+                child: Text("Press me!"),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                    backgroundColor: Colors.green
+                ),
+                onPressed: (){
+                  final load = AudioPlayer();
+                  load.play(AssetSource('note4.wav'));
+                },
+                child: Text("Press me!"),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                    backgroundColor: Colors.blue
+                ),
+                onPressed: (){
+                  final load = AudioPlayer();
+                  load.play(AssetSource('note5.wav'));
+                },
+                child: Text("Press me!"),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                    backgroundColor: Colors.indigo
+                ),
+                onPressed: (){
+                  final load = AudioPlayer();
+                  load.play(AssetSource('note6.wav'));
+                },
+                child: Text("Press me!"),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                    backgroundColor: Colors.purple
+                ),
+                onPressed: (){
+                  final load = AudioPlayer();
+                  load.play(AssetSource('note7.wav'));
+                },
+                child: Text("Press me!"),
+              ),
+            ],
           ),
         ),
       ),
